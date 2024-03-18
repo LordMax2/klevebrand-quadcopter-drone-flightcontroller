1faf5a4561052ab4440050dc78f8bd2137a438ef
Max Klevebrand <max.klevebrand@gmail.com>
2024-03-18 19:15:11 +0100

    Added documentation in the .ino file

--- DRONE_BNO085_FlightController_v4/DRONE_BNO085_FlightController_v4.ino
+++ DRONE_BNO085_FlightController_v4/DRONE_BNO085_FlightController_v4.ino
@@ -1,7 +1,7 @@
 #include <Arduino.h>
-
 #include "klevebrand_maxfly_drone.h"
 
+// Define the drone
 Drone drone;
 
 void setup() {
@@ -10,15 +10,20 @@ void setup() {
 
   while (!Serial);
 
+  // Startup the gyro, radio and motors
   drone.setup();
 }
 
 void loop() {
+  // Get the latest data from the gyroscope
   drone.updateGyro();
 
+  // Recive data from the reciver
   drone.reciverRecive();
 
+  // Check if connection is alive
   if(drone.lostConnection()) {
+    // If connection is dead, stop the drone
     drone.resetPID();
     drone.stopMotors();
 
@@ -26,9 +31,16 @@ void loop() {
     
     delay(100);
   } else {
-    drone.regulateThrottlePID();
+    // If connection is good, check if we should set the PID zero offset
     drone.setZero();
+
+    // Then calculate the PID stabilization
     drone.calculatePID();
+
+    // regulate the result of the PID calculation to not go over accepted limits of the ESCs
+    drone.regulateThrottlePID();
+
+    // Run the motors with the calculated PID throttle
     drone.runMotors();
   }
 }
