474a22d4b675489e0d2687cc8a86c1cc83a4d90e
Max Klevebrand <max.klevebrand@gmail.com>
2025-02-05 17:30:26 +0100

    Fix pwm_reciever interupt logic

--- lib/klevebrand_maxfly_drone/components/pwm_receiver/pwm_receiver.cpp
+++ lib/klevebrand_maxfly_drone/components/pwm_receiver/pwm_receiver.cpp
@@ -6,46 +6,59 @@ volatile int PwmReceiver::pulseWidths[CHANNEL_COUNT] = {0, 0, 0, 0, 0, 0, 0, 0};
 
 void PwmReceiver::begin()
 {
-    for (int i = 0; i < CHANNEL_COUNT; i++)
-    {
-        pinMode(channelNumberToGpioMapArray[i], INPUT);
-
-        // This delay is needed for some reason, otherwise the all of the read values are 0 for some reason.
-        delay(1000);
-
-        attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[i]), recordPinChangePulseWidth, CHANGE);
-    }
+    pinMode(channelNumberToGpioMapArray[0], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[0]), recordPinChangePulseWidthChannel1, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[1], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[1]), recordPinChangePulseWidthChannel2, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[2], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[2]), recordPinChangePulseWidthChannel3, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[3], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[3]), recordPinChangePulseWidthChannel4, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[4], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[4]), recordPinChangePulseWidthChannel5, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[5], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[5]), recordPinChangePulseWidthChannel6, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[6], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[6]), recordPinChangePulseWidthChannel7, CHANGE);
+    
+    pinMode(channelNumberToGpioMapArray[7], INPUT);
+    attachPCINT(digitalPinToPCINT(channelNumberToGpioMapArray[7]), recordPinChangePulseWidthChannel8, CHANGE);
 }
 
-void PwmReceiver::recordPinChangePulseWidth()
+void PwmReceiver::recordPinChangePulseWidth(int channelNumber)
 {
-    for (int i = 0; i < CHANNEL_COUNT; i++)
+    int channelNumberIndex = channelNumber - 1;
+
+    int pinState = digitalRead(channelNumberToGpioMapArray[channelNumberIndex]);
+
+    /*
+     * If pin-state is high, stamp down the millis for that pin.
+     */
+    if (pinState == HIGH)
     {
-        int pinState = digitalRead(channelNumberToGpioMapArray[i]);
+        pulseStartMicros[channelNumberIndex] = micros();
+        return;
+    }
 
-        /*
-         * If pin-state is high, stamp down the millis for that pin.
-         */
-        if (pinState == HIGH)
-        {
-            pulseStartMicros[i] = micros();
-            return;
-        }
-
-        /*
-         * If the pin state isnt HIGH, it is LOW.
-         *
-         * First we need to check that we have a currently "high" signal for the pin, if thats the case, we calculate
-         * the pulse width with the pulse-start millits timestamp subtracted by the current millis timestamp.
-         *
-         * Then we reset the pulse-start for this pin.
-         */
-        if (pulseStartMicros[i] != 0)
-        {
-            pulseWidths[i] = (int)(micros() - pulseStartMicros[i]);
-            pulseStartMicros[i] = 0;
-            return;
-        }
+    /*
+     * If the pin state isnt HIGH, it is LOW.
+     *
+     * First we need to check that we have a currently "high" signal for the pin, if thats the case, we calculate
+     * the pulse width with the pulse-start millits timestamp subtracted by the current millis timestamp.
+     *
+     * Then we reset the pulse-start for this pin.
+     */
+    if (pulseStartMicros[channelNumberIndex] != 0)
+    {
+        pulseWidths[channelNumberIndex] = (int)(micros() - pulseStartMicros[channelNumberIndex]);
+        pulseStartMicros[channelNumberIndex] = 0;
+        return;
     }
 }
 
--- lib/klevebrand_maxfly_drone/components/pwm_receiver/pwm_receiver.h
+++ lib/klevebrand_maxfly_drone/components/pwm_receiver/pwm_receiver.h
@@ -13,7 +13,15 @@ public:
     int getChannelValue(int channelNumber);
 
 private:
-    static void recordPinChangePulseWidth();
+    static void recordPinChangePulseWidth(int channelNumber);
+    static void recordPinChangePulseWidthChannel1() { recordPinChangePulseWidth(1); };
+    static void recordPinChangePulseWidthChannel2() { recordPinChangePulseWidth(2); };
+    static void recordPinChangePulseWidthChannel3() { recordPinChangePulseWidth(3); };
+    static void recordPinChangePulseWidthChannel4() { recordPinChangePulseWidth(4); };
+    static void recordPinChangePulseWidthChannel5() { recordPinChangePulseWidth(5); };
+    static void recordPinChangePulseWidthChannel6() { recordPinChangePulseWidth(6); };
+    static void recordPinChangePulseWidthChannel7() { recordPinChangePulseWidth(7); };
+    static void recordPinChangePulseWidthChannel8() { recordPinChangePulseWidth(8); };
     static volatile int channelNumberToGpioMapArray[CHANNEL_COUNT];
     static volatile unsigned long pulseStartMicros[CHANNEL_COUNT];
     static volatile int pulseWidths[CHANNEL_COUNT];
