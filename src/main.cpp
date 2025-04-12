340bf8c0da8f5466536b143e9b09c93d67c38873
Max Klevebrand <max.klevebrand@gmail.com>
2025-04-12 12:57:05 +0200

    Map reciever channels

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include "../lib/klevebrand_maxfly_drone/klevebrand_maxfly_drone.h"
 #include "../lib/klevebrand_maxfly_drone/components/pwm_receiver/pwm_receiver.h"
 
-Drone drone = Drone(6, 7, 2, 3, 1, 2, 3, 4, 5, 7, 8);
+Drone drone = Drone(6, 7, 2, 3, 3, 4, 2, 1, 7, 5, 8);
 PwmReceiver receiver;
 
 void setup()
@@ -22,7 +22,17 @@ void loop()
   drone.setPIDFromReceiver(receiver);
 
   // Temp debug print receiver channel one value
+  //Serial.print(receiver.getChannelValue(1));
+  //Serial.print(",");
+  //Serial.print(receiver.getChannelValue(2));
+  //Serial.print(",");
+  //Serial.print(receiver.getChannelValue(3));
+  //Serial.print(",");
+  //Serial.print(receiver.getChannelValue(4));
+  //Serial.print(",");
   //Serial.print(receiver.getChannelValue(5));
+  //Serial.print(",");
+  //Serial.print(receiver.getChannelValue(6));
   //Serial.print(",");
   //Serial.print(receiver.getChannelValue(7));
   //Serial.print(",");
