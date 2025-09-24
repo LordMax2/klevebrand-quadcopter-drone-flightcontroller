37cc3eefa9d047027189bfce2f8856dc21d6187e
LordMax2 <max.klevebrand@gmail.com>
2025-09-24 17:17:08 +0200

    Move .h file code to .cpp file

--- lib/klevebrand_maxfly_drone/components/quadcopter_pid/quadcopter_pid.cpp
+++ lib/klevebrand_maxfly_drone/components/quadcopter_pid/quadcopter_pid.cpp
@@ -61,3 +61,78 @@ void Pid::printPidConstants()
   Serial.print(",");
   Serial.println(pid_roll_optimizer.getKd());
 }
+
+float Pid::rollPid(float gyro_roll, float roll_desired_angle)
+{
+  return constrain(rollPidP(gyro_roll, roll_desired_angle) + roll_pid_i + rollPidD(gyro_roll, roll_desired_angle), -PID_MAX, PID_MAX);
+}
+
+float Pid::rollError(float gyro_roll, float roll_desired_angle)
+{
+  return roll_desired_angle - gyro_roll;
+}
+
+float Pid::rollPidP(float gyro_roll, float roll_desired_angle)
+{
+  return (pid_roll_optimizer.getKp()) * rollError(gyro_roll, roll_desired_angle);
+}
+
+float Pid::rollPidD(float gyro_roll, float roll_desired_angle)
+{
+  return (pid_roll_optimizer.getKd()) * (rollError(gyro_roll, roll_desired_angle) - roll_previous_error);
+}
+
+float Pid::pitchPid(float gyro_pitch, float pitch_desired_angle)
+{
+  return constrain(pitchPidP(gyro_pitch, pitch_desired_angle) + pitch_pid_i + pitchPidD(gyro_pitch, pitch_desired_angle), -PID_MAX, PID_MAX);
+}
+
+float Pid::pitchError(float gyro_pitch, float pitch_desired_angle)
+{
+  return pitch_desired_angle - gyro_pitch;
+}
+
+float Pid::pitchPidP(float gyro_pitch, float pitch_desired_angle)
+{
+  return pid_pitch_optimizer.getKp() * pitchError(gyro_pitch, pitch_desired_angle);
+}
+
+float Pid::pitchPidD(float gyro_pitch, float pitch_desired_angle)
+{
+  return pid_pitch_optimizer.getKd() * (pitchError(gyro_pitch, pitch_desired_angle) - pitch_previous_error);
+}
+
+float Pid::yawPid(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
+{
+  return constrain(yawPidP(gyro_yaw, yaw_desired_angle, yaw_compass_mode) + yawPidD(gyro_yaw, yaw_desired_angle, yaw_compass_mode), -PID_MAX, PID_MAX);
+}
+
+float Pid::yawError(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
+{
+  if (yaw_compass_mode)
+  {
+    auto error = fmod((gyro_yaw + 180), 360) - fmod((yaw_desired_angle + 180), 360);
+    float absolute_error = min(abs(error), 360 - abs(error));
+
+    if (error < 0)
+    {
+      return -1 * absolute_error;
+    }
+
+    return absolute_error;
+  }
+
+  return yaw_desired_angle - gyro_yaw;
+}
+
+float Pid::yawPidP(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
+{
+  return pid_yaw_optimizer.getKp() * yawError(gyro_yaw, yaw_desired_angle, yaw_compass_mode);
+}
+
+float yaw_pid_i = 0;
+
+float Pid::yawPidD(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
+{
+  return pid_yaw_optimizer.getKd() * (yawError(gyro_yaw, yaw_desired_angle, yaw_compass_mode) - yaw_previous_error);
+}
--- lib/klevebrand_maxfly_drone/components/quadcopter_pid/quadcopter_pid.h
+++ lib/klevebrand_maxfly_drone/components/quadcopter_pid/quadcopter_pid.h
@@ -71,92 +71,28 @@ private:
     bool yaw_compass_mode;
 
     /* Roll PID */
-    float rollPid(float gyro_roll, float roll_desired_angle)
-    {
-        return constrain(rollPidP(gyro_roll, roll_desired_angle) + roll_pid_i + rollPidD(gyro_roll, roll_desired_angle), -PID_MAX, PID_MAX);
-    }
-
-    float rollError(float gyro_roll, float roll_desired_angle)
-    {
-        return roll_desired_angle - gyro_roll;
-    }
-
+    float rollPid(float gyro_roll, float roll_desired_angle);
+    float rollError(float gyro_roll, float roll_desired_angle);
     float roll_previous_error = 0;
-
-    float rollPidP(float gyro_roll, float roll_desired_angle)
-    {
-        return (pid_roll_optimizer.getKp()) * rollError(gyro_roll, roll_desired_angle);
-    }
-
+    float rollPidP(float gyro_roll, float roll_desired_angle);
     float roll_pid_i = 0;
-
-    float rollPidD(float gyro_roll, float roll_desired_angle)
-    {
-        return (pid_roll_optimizer.getKd()) * (rollError(gyro_roll, roll_desired_angle) - roll_previous_error);
-    }
+    float rollPidD(float gyro_roll, float roll_desired_angle);
 
     /* Pitch PID */
-    float pitchPid(float gyro_pitch, float pitch_desired_angle)
-    {
-        return constrain(pitchPidP(gyro_pitch, pitch_desired_angle) + pitch_pid_i + pitchPidD(gyro_pitch, pitch_desired_angle), -PID_MAX, PID_MAX);
-    }
-
-    float pitchError(float gyro_pitch, float pitch_desired_angle)
-    {
-        return pitch_desired_angle - gyro_pitch;
-    }
-
+    float pitchPid(float gyro_pitch, float pitch_desired_angle);
+    float pitchError(float gyro_pitch, float pitch_desired_angle);
     float pitch_previous_error = 0;
-
-    float pitchPidP(float gyro_pitch, float pitch_desired_angle)
-    {
-        return pid_pitch_optimizer.getKp() * pitchError(gyro_pitch, pitch_desired_angle);
-    }
-
+    float pitchPidP(float gyro_pitch, float pitch_desired_angle);
     float pitch_pid_i = 0;
-
-    float pitchPidD(float gyro_pitch, float pitch_desired_angle)
-    {
-        return pid_pitch_optimizer.getKd() * (pitchError(gyro_pitch, pitch_desired_angle) - pitch_previous_error);
-    }
+    float pitchPidD(float gyro_pitch, float pitch_desired_angle);
 
     /* Yaw PID */
-    float yawPid(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
-    {
-        return constrain(yawPidP(gyro_yaw, yaw_desired_angle, yaw_compass_mode) + yawPidD(gyro_yaw, yaw_desired_angle, yaw_compass_mode), -PID_MAX, PID_MAX);
-    }
-
-    float yawError(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
-    {
-        if (yaw_compass_mode)
-        {
-            auto error = fmod((gyro_yaw + 180), 360) - fmod((yaw_desired_angle + 180), 360);
-            float absolute_error = min(abs(error), 360 - abs(error));
-
-            if (error < 0)
-            {
-                return -1 * absolute_error;
-            }
-
-            return absolute_error;
-        }
-
-        return yaw_desired_angle - gyro_yaw;
-    }
-
+    float yawPid(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode);
+    float yawError(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode);
     float yaw_previous_error = 0;
-
-    float yawPidP(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
-    {
-        return pid_yaw_optimizer.getKp() * yawError(gyro_yaw, yaw_desired_angle, yaw_compass_mode);
-    }
-
+    float yawPidP(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode);
     float yaw_pid_i = 0;
-
-    float yawPidD(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode)
-    {
-        return pid_yaw_optimizer.getKd() * (yawError(gyro_yaw, yaw_desired_angle, yaw_compass_mode) - yaw_previous_error);
-    }
+    float yawPidD(float gyro_yaw, float yaw_desired_angle, bool yaw_compass_mode);
 };
 
 #endif
