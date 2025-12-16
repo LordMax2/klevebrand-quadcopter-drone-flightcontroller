fc500e2fddde1732d29c8dcbf10b1d5a5c4a0f38
LordMax2 <max.klevebrand@gmail.com>
2025-12-16 18:54:07 +0100

    Cleanup

--- lib/klevebrand_maxfly_drone/klevebrand_maxfly_drone.h
+++ lib/klevebrand_maxfly_drone/klevebrand_maxfly_drone.h
@@ -5,20 +5,9 @@
 #include "../klevebrand_drone_core/components/base_four_motor_drone/base_four_motor_drone.h"
 #include "./components/quadcopter_pid/quadcopter_pid.h"
 
-#define TRANSMITION_TIMEOUT_DEFINITION_MILLISECONDS 500
-
-#define SERIAL_BAUD_RATE 115200
-
-#define FEEDBACK_LOOP_HZ 200
-
-#define PID_PERSIST_INTERVAL_MILLISECONDS 10000
-
 class KlevebrandMaxFlyDrone : public BaseFourMotorDrone<QuadcopterPid>
 {
 public:
-  /*
-   * Create a drone
-   */
   KlevebrandMaxFlyDrone(
       uint8_t motor_left_front_pin_number,
       uint8_t motor_right_front_pin_number,
--- lib/klevebrand_maxjet_drone/klevebrand_maxjet_drone.h
+++ lib/klevebrand_maxjet_drone/klevebrand_maxjet_drone.h
@@ -5,20 +5,9 @@
 #include "../klevebrand_drone_core/components/base_four_motor_drone/base_four_motor_drone.h"
 #include "./components/airplane_vtail_pid/airplane_vtail_pid.h"
 
-#define TRANSMITION_TIMEOUT_DEFINITION_MILLISECONDS 500
-
-#define SERIAL_BAUD_RATE 115200
-
-#define FEEDBACK_LOOP_HZ 200
-
-#define PID_PERSIST_INTERVAL_MILLISECONDS 10000
-
 class KlevebrandMaxJetDrone : public BaseFourMotorDrone<AirplaneVtailPid>
 {
 public:
-    /*
-     * Create a drone
-     */
     KlevebrandMaxJetDrone(
         uint8_t motor_left_front_pin_number,
         uint8_t motor_right_front_pin_number,
